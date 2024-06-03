292686938b69f7587d03160cb3bb773f4d82c9cb
izhar <150798894+IZHARaja@users.noreply.github.com>
2024-06-03 12:02:28 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+function myFunction() {
+	document.getElementById("1").innerHTML = "IZHAR";
+	document.getElementById("2").innerHTML = "MAIN KUCING";
+	document.getElementById("3").innerHTML = "ISSEKAI";
+}
